34f1980c6aa038af1e45212635ec447740c118c6
Daniel Beaver <daniel.beaver1012@gmail.com>
2020-04-15 14:11:44 -0400

    heroku push

--- models/user.js
+++ models/user.js
@@ -2,26 +2,26 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     lastName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     userType: {
       type: DataTypes.ENUM("User", "Vendor", "Admin"),
       allowNull: false,
-      defaultValue: "User"
-    }
+      defaultValue: "User",
+    },
   });
 
   return User;
